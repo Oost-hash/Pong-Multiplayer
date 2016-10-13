a601d4f5cdd3371938b6d77fdc4a792f00a7b7b5
Rob-sama <rroeten@gmail.com>
2016-10-13 13:18:18 +0200

    Code cleanup
    Did some cleanup to the server.js

--- pong - v0.3/serverV0.2.js
+++ pong - v0.3/serverV0.2.js
@@ -6,49 +6,41 @@
  **************************************************
  * REQUIREMENTS
  **************************************************/
+var util = require("util"),                                     // Module for logging
+    express = require('express'),                               // Module for express framework
+    http = require('http'),                                     // Module for http server
+    socketio = require('socket.io');                            // Module for Socket.io
 
-var util = require("util"),                 // Module for logging
-    express = require('express'),           // Module for express framework
-    http = require('http'),                 // using http
-    socketio = require('socket.io');        // loads up socket.io
-
-var port = 3000;                            // port
-var players = [];                           // array for players
-var waitingLine = [];                       // array for waiting players
+var port = 3000;                                                // Default Port
+var players = [];                                               // Array for players
+var waitingLine = [];                                           // Array for waiting players
 
 /**************************************************
  ** SERVER SERTUP
  **************************************************/
-var app = express(),                            // create express instance
-    server = http.createServer(app),            // create the http server from express
-    io = socketio(server);                      // setup socket io to listen to the server
+var app = express(),                                            // Create express instance
+    server = http.createServer(app),                            // Create the http server from express
+    io = socketio(server);                                      // Setup socket io to listen to the server
 
-var nspClient = io.of('/client');               //Name space for connecting clients
-app.use('/public', express.static(__dirname  + '/public'));   //Css an js access for node
+var nspClient = io.of('/client');                               // Name space for connecting clients
+app.use('/public', express.static(__dirname  + '/public'));     // Public folders
 
 //Redirects to pong !!need to change it to start
 app.get('/', function(request, response){
-    response.sendFile(__dirname + '/pong.html');
+    //noinspection JSUnresolvedFunction
+    response.sendFile(__dirname + '/pong.html');                // Index file on connect
 });
 
 /**************************************************
- ** Initialize server
+ ** Event Handlers
  **************************************************/
-
-function init() {
-    server.listen(port, function () {
-        util.log('Server Started listing on port: 3000')
-    });
-
-    nspClient.on('connection', clientConnected);
-    nspClient.on('disconnect', clientDisconnect)
-}
 
 function clientDisconnect(client){
     client.leave('room1');
     util.log('Client disconnected: ' + client.id)
 }
 
+//test function
 function clientConnected(client){
     util.log('Client joined ' + client.id);
     enterRoom(client);
@@ -89,15 +81,19 @@ function enterRoom(client){
 
 function play(room) {
     // nspClient.in(room).emit('test', 'msg');
-};
+}
 
+/**************************************************
+ ** Initialize server
+ **************************************************/
+
+function init() {
+    server.listen(port, function () {
+        util.log('Server Started listing on port: 3000')
+    });
+
+    nspClient.on('connection', clientConnected);
+    nspClient.on('disconnect', clientDisconnect)
+}
 
 init();
-
-//idee om posities door te geven:
-// util.log('client id: '  +client.id + ' PosY: ' + paddleY.pos);
-// if(client.id != players[0]){
-//     players[0].emit('movePlayer', {pos: paddleY.pos, id: client.id});
-// } else{
-//     players[1].emit('movePlayer', {pos: paddleY.pos, id: client.id});
-// }
